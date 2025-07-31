7b33180a4583c66b3d0a2c8278c3fd8376ac72fd
singhdevendrajtz <singhdevendrajtz@gmail.com>
2025-07-31 16:05:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
 function convertToRoman(num) {
   	const obj = {
       0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
+      1:['CM',900],
+      2:['D',500],
+      3:['CD',400], 
+      4:['C',100], 
+      5:['XC',90],
+      6:['L',50],
+      7:['XL', 40], 
+      8:['X',10],
+      9:['IX', 9], 
+      10:['V',5], 
+      11:['IV',4],
+      12:['I',1]
     };
+    let final_arr=[];
+for(let key in obj)
+{ //console.log(key)
+    let qot=null;
+    if(obj[key][1]<=num )
+    {
+       
+         qot=num/(obj[key][1]);
+         qot=Math.floor(qot);
+        // console.log(qot)
+         num=num-(obj[key][1]*qot)
+         let count=0;
+         while(count<qot)
+         {
+             final_arr.push(obj[key][0]);
+             count++;
+         }
+        
+    }
+    
+  //your code here
+
+}
+return(final_arr.join(''));
+	
 
   //your code here
 
